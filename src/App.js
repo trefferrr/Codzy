374527be3278630d46904f8f3ef729602385ef4e
trefferrr <trefferx07@gmail.com>
2024-07-08 14:15:34 +0530

    src commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import CodeEditor from './Components/CodeEditor';
+import Landing from './Components/Landing';
+
+function App() {
+  return (
+   <Landing/>
+   
+  );
+}
+
+export default App;
